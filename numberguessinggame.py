8a88511da34a268c26c193fa9497984469d67126
Koshal67 <71601763+Koshal67@users.noreply.github.com>
2020-10-02 12:34:13 +0530

    Completely updated numberguessinggame.py
    Hello ArjunSahlot ,
    I have updated numberguessinggame.py . Earlier your game was so small and not so interactive . But now , it is more reliable . 
    I think you must merge it .
    Thanks
    github.com/Koshal67/

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -1,18 +1,24 @@
 import random
+computer_no=random.randint(0,10)
+print("Do you ready for the challenge " )
+user = str(input("[ Y/n ] " ))
+user2 = user.lower()
+if user2 == "y" :
+    def sameno(target,number):
+        if target == number:
+            result = "You Win"
+           
+        else:
+            result="Wrong !! Try again : "
+        return result
+    print("You have to guess numbers between 0 and 10 ")
+    print("Let's guess a number")
+    guess=int(input("What is your guess?: "))
+    print(sameno(computer_no,guess))
+   
 
-computer_no=random.randint(1,5)
 
-def sameno(target,number):
-    if target == number:
-        result = "You Win"
 
-    else:
-        result="Take the L"
-
-    return result
-
-print("Let's guess a number")
-
-guess=int(input("What is your guess?: "))
-
-print(sameno(computer_no,guess))
+if user2 == "n" :
+    print("AS your choice : Thanks for playing : ")
+                                    
